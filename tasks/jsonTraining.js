22f45b3eda7a192ad5fba0bcc5f1525a10e7ee12
floork <floork@gmx.de>
2022-11-25 13:52:57 +0100

    final commit of the course

--- tasks/jsonTraining.js
+++ tasks/jsonTraining.js
@@ -1,18 +1,19 @@
+{/* <script> */}
   fetch('/~jitschin/weather.json')
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < data.length; i++){
         let obj = data[i];
+        var temp = new Date(parseInt(obj.timestamp) * 1000);
         var tableRow = document.createElement("tr");
         var tableCell = document.createElement("td");
         var table = document.getElementById('table');
+        var date = temp.getDate() + "." + temp.getMonth() + "." + temp.getFullYear();
+        var hour = temp.getHours(); //FIXME: is an Int and has to be a string to give the correct hour
         table.appendChild(tableRow);
         tableRow.appendChild(tableCell);
-        tableCell.innerText = "Temperature: " + obj.temp + "°C";
+        tableCell.innerText = "Temperature: " + obj.temp + "°C" + ", " + "Date: " + date + ", " + hour + " o'clock";
       }
-      var datum = data[i];
-
-            var date = new Date(parseInt(datum.timestamp) * 1000);
-            var dateFormatted = date.toLocaleString();
-
     });
+{/* </script>
+<table id="table"></table> */}
